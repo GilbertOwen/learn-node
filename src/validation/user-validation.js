ac42eab7a939fd565178a9684dd26db48808e8bb
Gilbert Owen <psychoganko@gmail.com>
2024-12-01 15:15:43 +0700

    feat: Create opsional update user validation

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -7,8 +7,14 @@ const registerUserValidationSchema = z.object({
 });
 
 const loginUserValidationSchema = z.object({
-  username: z.string(),
-  password: z.string(),
+  username: z.string().nonempty("Username is required"),
+  password: z.string().nonempty("Password is required"),
 });
 
-export { registerUserValidationSchema, loginUserValidationSchema };
+const updateUserValidationSchema = z.object({
+  username: z.string().min(6, "Username must be at least 6 characters long"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+}).partial();
+
+export { registerUserValidationSchema, loginUserValidationSchema, updateUserValidationSchema };
