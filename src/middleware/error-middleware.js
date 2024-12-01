1813b8aa3228f95a77f040f88d628d2237e066dc
Gilbert Owen <psychoganko@gmail.com>
2024-12-01 13:01:18 +0700

    feat: Created middleware for error handling whether it is a Response Error, Zod Error, and server error

---
+++ src/middleware/error-middleware.js
@@ -0,0 +1,22 @@
+import { ResponseError } from "../error/response-error.js";
+import { ZodError } from "zod";
+
+export const errorMiddleware = async (err, req, res, next) => {
+    if (!err) {
+        next();
+        return;
+    }
+    if (err instanceof ResponseError) {
+        res.status(err.status)
+           .json({ message: err.message, errors: err.message })
+           .end();
+    } else if (err instanceof ZodError) {
+        res.status(400)
+           .json({ message: "Failed to insert user's information, please try again", errors: err.errors })
+           .end();
+    } else {
+        res.status(500)
+           .json({ message: "Internal Server Error" })
+           .end();
+    }
+};
