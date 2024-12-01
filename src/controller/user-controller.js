d56194efb1e1c447c8d7e575a8e9321b85bc4d64
Gilbert Owen <psychoganko@gmail.com>
2024-12-01 15:19:39 +0700

    feat: Added method for controller to handle get user information route endpoint 'api/auth/me'

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -24,4 +24,16 @@ const login = async (req, res, next) => {
   }
 };
 
-export default { register, login };
+const current = async (req, res, next) => {
+  try {
+    const result = await userService.me(req.user);
+    res.status(200).json({
+      data: result,
+      message: "Successfully retrieved user's information",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { register, login, current };
