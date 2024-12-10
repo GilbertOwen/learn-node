e8a9e9223a369a1a536de33a5c9590d8e224e32e
Gilbert Owen <psychoganko@gmail.com>
2024-12-10 11:57:10 +0700

    refactor: added errors key and value to ResponseError response return

--- src/error/response-error.js
+++ src/error/response-error.js
@@ -1,6 +1,7 @@
 class ResponseError extends Error {
-    constructor(statusCode, message) {
+    constructor(statusCode, message,errors = null) {
         super(message);
+        this.errors = errors;
         this.status = statusCode;
     }
 }
