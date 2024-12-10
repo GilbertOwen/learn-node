71f51e6c9a96b7e91ef95c588eb986e989ab775a
Gilbert Owen <psychoganko@gmail.com>
2024-12-10 11:52:20 +0700

    feat: add cors origin for public ip

--- src/application/index.js
+++ src/application/index.js
@@ -4,6 +4,15 @@ import { errorMiddleware } from "../middleware/error-middleware.js";
 import { userRouter } from "../route/protected-api.js";
 const app = express();
 app.use(express.json());
+import cors from "cors";
+
+app.options("*", cors()); // Preflight request support
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://159.65.9.173:3000"], // Front-end URL
+  })
+);
 
 app.use(publicRouter);
 app.use(userRouter);
