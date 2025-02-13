258a9262f52fb9b99fbe5a006477d6fa7b0b44f1
Gilbert Owen <psychoganko@gmail.com>
2025-02-13 22:47:32 +0700

    feat: added more validation for user profile update

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -4,6 +4,8 @@ const registerUserValidationSchema = z.object({
   username: z.string().min(6, "Username must be at least 6 characters long"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters long"),
+  frontName: z.string().min(4, "Front name must be at least 4 characters long"),
+  rearName: z.string().nullable(),
 });
 
 const loginUserValidationSchema = z.object({
@@ -11,10 +13,22 @@ const loginUserValidationSchema = z.object({
   password: z.string().nonempty("Password is required"),
 });
 
-const updateUserValidationSchema = z.object({
-  username: z.string().min(6, "Username must be at least 6 characters long"),
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
-}).partial();
+const updateUserValidationSchema = z
+  .object({
+    username: z.string().min(6, "Username must be at least 6 characters long"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    rearName: z.string(),
+    frontName: z
+      .string()
+      .min(3, "Front name must be at least 3 characters long"),
+    biodata: z.string(),
+    imageUrl: z.string(),
+  })
+  .partial();
 
-export { registerUserValidationSchema, loginUserValidationSchema, updateUserValidationSchema };
+export {
+  registerUserValidationSchema,
+  loginUserValidationSchema,
+  updateUserValidationSchema,
+};
